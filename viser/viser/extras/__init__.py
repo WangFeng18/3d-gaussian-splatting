ea5963c3e77cfc0fac8fb4b75eb9011fbe76c43f
wangfeng <wangfengwork17@gmail.com>
2023-06-18 16:38:29 +0800

    sync

--- viser/viser/extras/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Extra utilities. Used for example scripts."""
-
-from ._record3d import Record3dFrame as Record3dFrame
-from ._record3d import Record3dLoader as Record3dLoader
--- viser/viser/transforms/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._utils import get_epsilon, register_lie_group
-
-__all__ = ["get_epsilon", "register_lie_group"]
